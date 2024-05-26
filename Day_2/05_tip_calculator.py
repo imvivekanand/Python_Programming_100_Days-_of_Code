b94a2f57efd8801925366029e492e5829f943da9
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2024-05-26 08:49:57 +0530

    Day 3 started

--- Day_2/05_tip_calculator.py
+++ Day_2/05_tip_calculator.py
@@ -1,5 +1,5 @@
 print("Welcome to the tip calculator!")
-total_bill = float(input("What was the total bill?"))
+total_bill = float(input("What was the total bill? ₹"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
 people = int(input("How many people to split the bill?"))
 tip_as_percent = tip / 100
---
+++ Day_3/01_checkheight.py
@@ -0,0 +1,7 @@
+def rollerCoster(height):
+    if height > 120:
+        print("You can ride the rollercoaster!")
+    else:
+        print("You cannot ride the rollercoaster")
+
+rollerCoster(167)
