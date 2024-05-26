e54a14c1f4c1edefb9ab6c665c793918e6d43c6c
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2024-05-26 12:27:52 +0530

    pizza billing

---
+++ Day_3/03_bmi_calculator_2.0.py
@@ -0,0 +1,16 @@
+height = float(input())
+weight = int(input())
+
+sq_height = height ** 2
+
+BMI = weight/sq_height
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+  print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese.")
